1f1925d5947502b8b8b2877455288b58aef4180b
Wanming Lin <wanming.lin@intel.com>
2017-10-21 15:49:03 +0800

    Debug app manifest

---
+++ tests/appmanifest/sw.js
@@ -0,0 +1,5 @@
+'use strict';
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener("fetch", () => {});
