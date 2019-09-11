b544990e36cded16ee5b2e4c9566e4558c6d4f76
Wanming Lin <wanming.lin@intel.com>
2019-09-11 16:28:59 +0800

    Optimize nfc sample

--- web-nfc/script.js
+++ web-nfc/script.js
@@ -1,36 +1,38 @@
 if (!NFCReader) {
-  pre.textContent += `Error: ${error}\n`;
+  pre.innerHTML += `<font color="red">Error: ${error}</font>\n`;
 }
 
 const r = new NFCReader();
 
 r.onerror = event => {
-  pre.textContent += "Error: " + event.error + "\n";
+  pre.innerHTML += "<font color='red'>Error: " + event.error + "</font>\n";
 };
 
 const onReading = ({ message }) => {
-  pre.textContent += `> Reading from ${event.serialNumber}\n`;
-  pre.textContent += `> URL: ${message.url}\n`;
-  pre.textContent += `> Records:\n`;
+  pre.innerHTML += `<br>> <font color='green'>Reading from ${event.serialNumber}</font>\n`;
+  pre.innerHTML += `> <font color='green'>Records:</font>\n`;
 
   if (message.records.length === 0) {
-    pre.textContent += `  > No WebNFC records\n`;
+    pre.innerHTML += `  > <font color='green'>No WebNFC records</font>\n`;
     return;
   }
 
   for (const record of message.records) {
+    pre.innerHTML += `  > <font color='green'>recordType: ${record.recordType}</font>\n`;
+    pre.innerHTML += `  > <font color='green'>mediaType: ${record.mediaType}</font>\n`;
+    pre.innerHTML += `  > <font color='green'>id: ${record.id}</font>\n`;
     switch (record.recordType) {
       case "empty":
-        pre.textContent += `  > Empty record\n`;
+        pre.innerHTML += `  > <font color='green'>data: Empty record</font>\n`;
         break;
       case "text":
-        pre.textContent += `  > Text: ${record.toText()}\n`;
+        pre.innerHTML += `  > <font color='green'>data: ${record.toText()}</font>\n`;
         break;
       case "url":
-        pre.textContent += `  > URL: ${record.toText()}\n`;
+        pre.innerHTML += `  > <font color='green'>data: ${record.toText()}</font>\n`;
         break;
       case "json":
-        pre.textContent += `  > JSON: ${JSON.stringify(record.toJSON())}\n`;
+        pre.innerHTML += `  > <font color='green'>data: ${JSON.stringify(record.toJSON())}</font>\n`;
         break;
       case "opaque":
         if (record.mediaType.startsWith("image/")) {
@@ -39,11 +41,15 @@ const onReading = ({ message }) => {
           const img = document.createElement("img");
           img.src = URL.createObjectURL(blob);
           img.onload = () => window.URL.revokeObjectURL(this.src);
+          img.style.paddingLeft = "15px";
 
-          document.body.appendChild(img);
+          pre.innerHTML += `  > <font color='green'>data:</font>\n`;
+          pre.appendChild(img);
+
+          const br = document.createElement("br");
+          pre.appendChild(br);
         } else {
-
-
+          pre.innerHTML += `  > <font color='green'>data: ${record.toArrayBuffer()}</font>\n`;
         }
         break;
     }
@@ -60,15 +66,16 @@ onReadingInputChange();
 /* Scan signal */
 const abortController = new AbortController();
 abortController.signal.addEventListener("abort", _ => {
-  pre.textContent += "> Aborted scan\n";
+  pre.innerHTML += "> <font color='green'>Scan operation aborted.</font>\n";
 });
-pre.textContent += `Reading need scan...\n`;
+pre.innerHTML += `<b>Note: Click "scan" button to start reading.</b>\n`;
 
 abortButton.addEventListener("click", _ => {
   abortController.abort();
 });
 
 scanButton.addEventListener("click", _ => {
+  pre.innerHTML += "<b>Start scanning...</b>\n";
   if (recordType.value === "---") {
     r.scan({
       signal: abortController.signal,
@@ -88,7 +95,7 @@ scanButton.addEventListener("click", _ => {
 /* Push signal */
 const abortPushController = new AbortController();
 abortPushController.signal.addEventListener("abort", _ => {
-  pre.textContent += "> Aborted push\n";
+  pre.innerHTML += "> <font color='green'>Push operation aborted.</font>\n";
 });
 
 abortPushButton.addEventListener("click", _ => {
@@ -97,7 +104,7 @@ abortPushButton.addEventListener("click", _ => {
 
 /* Push Message */
 pushMessage.value = '{ "records": [{ "recordType": "text", "data": "hello", \
-    "mediaType": "text/plain", "id": "1" }] }';
+"mediaType": "text/plain", "id": "1" }] }';
 const opaque_data = new ArrayBuffer(15);
 pushRecordType.onchange = () => {
   pushMessage.disabled = false;
@@ -107,21 +114,21 @@ pushRecordType.onchange = () => {
       break;
     case "text":
       pushMessage.value = '{ "records": [{ "recordType": "text", \
-          "data": "hello", "mediaType": "text/plain", "id": "1" }] }';
+"data": "hello", "mediaType": "text/plain", "id": "1" }] }';
       break;
     case "url":
       pushMessage.value = '{ "records": [{ "recordType": "url", "data": \
-           "http://www.intel.com", "mediaType": "text/plain", "id": "1" }] }';
+"http://www.intel.com", "mediaType": "text/plain", "id": "1" }] }';
       break;
     case "json":
       pushMessage.value = '{ "records": [{ "recordType": "json", \
-          "data": { "key1": "value1", "key2": "value2" }, \
-          "mediaType": "application/json", "id": "1" }] }';
+"data": { "key1": "value1", "key2": "value2" }, \
+"mediaType": "application/json", "id": "1" }] }';
       break;
     case "opaque":
       pushMessage.value = '{ "records": [{ "recordType": "opaque", \
-          "data": [object ArrayBuffer], "mediaType": "application/octet-stream"\
-          , "id": "1" }] }';
+"data": [object ArrayBuffer], "mediaType": "application/octet-stream"\
+, "id": "1" }] }';
       pushMessage.disabled = true;
       break;
   }
@@ -129,18 +136,16 @@ pushRecordType.onchange = () => {
 
 /* Write */
 
-const test_text_data = 'Test text data.';
-const test_text_byte_array = new TextEncoder('utf-8').encode(test_text_data);
-const test_buffer_data = new ArrayBuffer(test_text_byte_array.length);
-const test_buffer_view =
-    new Uint8Array(test_buffer_data).set(test_text_byte_array);
 writeButton.addEventListener("click", async _ => {
-  pre.textContent += "Writing...\n";
+  pre.innerHTML += "<b>Start writing...</b>\n";
   const w = new NFCWriter();
   let newMessage = null;
   if (pushRecordType.value ===  "opaque") {
+    const response = await fetch("./green.png");
+    const test_buffer_data = await response.arrayBuffer();
+
     newMessage = { records: [{ recordType: "opaque", data: test_buffer_data,
-        mediaType: "application/octet-stream", id: "1" }] };
+        mediaType: "image/png", id: "1" }] };
   } else {
     newMessage = JSON.parse(pushMessage.value);
   }
@@ -154,9 +159,9 @@ writeButton.addEventListener("click", async _ => {
         signal: abortPushController.signal
       }
     );
-    pre.textContent += "> Written\n";
+    pre.innerHTML += "> <font color='green'>Writing data successfully.</font>\n";
   } catch(e) {
-    pre.textContent += `> ${e}\n`;
+    pre.innerHTML += `> <font color='red'>${e}</font>\n`;
   }
 });
 
