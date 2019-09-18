b03986464e4fc04962a159efcd639527ad5c5231
Wanming Lin <wanming.lin@intel.com>
2019-09-18 16:53:19 +0800

    NFC sample optimzation once again

--- web-nfc/script.js
+++ web-nfc/script.js
@@ -8,7 +8,7 @@ r.onerror = event => {
   pre.innerHTML += "<font color='red'>Error: " + event.error + "</font>\n";
 };
 
-const onReading = ({ message }) => {
+r.onreading = ({ message }) => {
   pre.innerHTML += `<br>> <font color='green'>Reading from ${event.serialNumber}</font>\n`;
   pre.innerHTML += `> <font color='green'>Records:</font>\n`;
 
@@ -35,33 +35,21 @@ const onReading = ({ message }) => {
         pre.innerHTML += `  > <font color='green'>data: ${JSON.stringify(record.toJSON())}</font>\n`;
         break;
       case "opaque":
-        if (record.mediaType.startsWith("image/")) {
-          const blob = new Blob([record.toArrayBuffer()], {type: record.mediaType});
+        const blob = new Blob([record.toArrayBuffer()], {type: record.mediaType});
 
-          const img = document.createElement("img");
-          img.src = URL.createObjectURL(blob);
-          img.onload = () => window.URL.revokeObjectURL(this.src);
-          img.style.paddingLeft = "15px";
+        const img = document.createElement("img");
+        img.src = URL.createObjectURL(blob);
+        img.style.paddingLeft = "15px";
 
-          pre.innerHTML += `  > <font color='green'>data:</font>\n`;
-          pre.appendChild(img);
+        pre.innerHTML += `  > <font color='green'>data:</font>\n`;
+        pre.appendChild(img);
 
-          const br = document.createElement("br");
-          pre.appendChild(br);
-        } else {
-          pre.innerHTML += `  > <font color='green'>data: ${record.toArrayBuffer()}</font>\n`;
-        }
+        const br = document.createElement("br");
+        pre.appendChild(br);
         break;
     }
   }
 };
-
-const onReadingInputChange = _ => {
-  r.onreading = readingInput.checked ? onReading : null;
-}
-
-readingInput.onchange = onReadingInputChange;
-onReadingInputChange();
 
 /* Scan signal */
 const abortController = new AbortController();
@@ -103,8 +91,29 @@ abortPushButton.addEventListener("click", _ => {
 });
 
 /* Push Message */
-pushMessage.value = '{ "records": [{ "recordType": "text", "data": "hello", \
-"mediaType": "text/plain", "id": "1" }] }';
+pushMessage.value = 'Hello web-nfc';
+NDEFMessageInit.style.display="none";
+
+NDEFMessageSource.onchange = () => {
+  pushMessage.disabled = false;
+  NDEFMessageInit.style.display="none";
+  switch (NDEFMessageSource.value) {
+    case "DOMString":
+      pushMessage.value = 'Hello web-nfc';
+      break;
+    case "ArrayBuffer":
+      pushMessage.value = 'fetch("./red.png").arrayBuffer()';
+      pushMessage.disabled = true;
+      break;
+    case "NDEFMessageInit":
+      pushRecordType.value = "text";
+      pushMessage.value = '{ "records": [{ "recordType": "text", "data": "hello", \
+"mediaType": "text/plain", "id": "http://www.intel.com" }] }';
+      NDEFMessageInit.style.display="";
+      break;
+  }
+}
+
 const opaque_data = new ArrayBuffer(15);
 pushRecordType.onchange = () => {
   pushMessage.disabled = false;
@@ -114,21 +123,21 @@ pushRecordType.onchange = () => {
       break;
     case "text":
       pushMessage.value = '{ "records": [{ "recordType": "text", \
-"data": "hello", "mediaType": "text/plain", "id": "1" }] }';
+"data": "hello", "mediaType": "text/plain", "id": "http://www.intel.com" }] }';
       break;
     case "url":
       pushMessage.value = '{ "records": [{ "recordType": "url", "data": \
-"http://www.intel.com", "mediaType": "text/plain", "id": "1" }] }';
+"http://www.intel.com", "mediaType": "text/plain", "id": "http://www.intel.com" }] }';
       break;
     case "json":
       pushMessage.value = '{ "records": [{ "recordType": "json", \
 "data": { "key1": "value1", "key2": "value2" }, \
-"mediaType": "application/json", "id": "1" }] }';
+"mediaType": "application/json", "id": "http://www.intel.com" }] }';
       break;
     case "opaque":
       pushMessage.value = '{ "records": [{ "recordType": "opaque", \
-"data": [object ArrayBuffer], "mediaType": "application/octet-stream"\
-, "id": "1" }] }';
+"data": fetch("./green.png").arrayBuffer(), "mediaType": "application/octet-stream"\
+, "id": "http://www.intel.com" }] }';
       pushMessage.disabled = true;
       break;
   }
@@ -140,12 +149,19 @@ writeButton.addEventListener("click", async _ => {
   pre.innerHTML += "<b>Start writing...</b>\n";
   const w = new NFCWriter();
   let newMessage = null;
-  if (pushRecordType.value ===  "opaque") {
+  if (NDEFMessageSource.value ===  "DOMString") {
+    newMessage = pushMessage.value;
+  } else if (NDEFMessageSource.value === "ArrayBuffer") {
+    const response = await fetch("./red.png");
+    const test_buffer_data = await response.arrayBuffer();
+
+    newMessage = test_buffer_data;
+  } else if (NDEFMessageSource.value === "NDEFMessageInit" && pushRecordType.value ===  "opaque") {
     const response = await fetch("./green.png");
     const test_buffer_data = await response.arrayBuffer();
 
     newMessage = { records: [{ recordType: "opaque", data: test_buffer_data,
-        mediaType: "image/png", id: "1" }] };
+        mediaType: "application/octet-stream", id: "http://www.intel.com" }] };
   } else {
     newMessage = JSON.parse(pushMessage.value);
   }
